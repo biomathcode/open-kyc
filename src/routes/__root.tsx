664650549a0edc4aa9548b8017afbcb2ac4fa039
Pratik Sharma <sharma.pratik2016@gmail.com>
2025-11-13 17:18:57 +0530

    fix: animated landing page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -51,7 +51,7 @@ export const Route = createRootRouteWithContext<{
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'OpeKYC- Platform for Identity Verification, Onboarding, KYC, and more',
       },
     ],
     links: [
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Navbar } from '~/components/landing'
+import Client from '~/components/landing/client'
 import CTA from '~/components/landing/cta'
 
 
@@ -11,10 +12,11 @@ function Home() {
 
 
   return (
-    <>
+    <div className="flex flex-col   w-full">
       <Navbar />
       <CTA />
-    </>
+      <Client />
+    </div>
   )
 }
 
